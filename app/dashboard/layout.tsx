2f4e3f8ec03b0d9cbf2729d9844c5d868411bca7
Colton923 <110353948+Colton923@users.noreply.github.com>
2022-11-30 19:30:41 -0500

    pray

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,27 @@
 import styles from '../../styles/App.module.css'
 import DashboardNavbar from './DashboardNavbar'
-
+import Products from './Products'
 export default async function Layout({ children }: any) {
   return (
     <div>
       <div className={styles.main}>
         <DashboardNavbar />
+        <div className={styles.leftcontainer}>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+          <h1>Boring</h1>
+        </div>
+        <div className={styles.rightcontainer}>
+          <div className={styles.cardwrapper}>
+            <Products />
+          </div>
+        </div>
       </div>
       <div>{children}</div>
     </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,7 @@
-import styles from '../../styles/Dashboard.module.css'
-import Products from './Products'
-
 export default function Page() {
   return (
-    <div className={styles.main}>
-      <div className={styles.leftcontainer}>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-        <h1>Boring</h1>
-      </div>
-      <div className={styles.rightcontainer}>
-        <div className={styles.cardwrapper}>
-          <Products />
-        </div>
-      </div>
+    <div>
+      <h1>Dashboard</h1>
     </div>
   )
 }
