08b066cf31a9243b87426f8d73d116564a791ecd
Karsten Koerner <kgk1999@gmail.com>
2022-12-22 02:04:03 -0500

    dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,33 @@
 'use client'
-import styles from '../../styles/PlayerDashboard.module.css'
+import Image from 'next/image'
+import imgBackground from '../../public/static/images/tgf_dashboard_background.png'
+import styles from '../../styles/Dashboard_new.module.css'
+import AdminNavbar from '../../components/AdminNavbar'
+import { useEffect } from 'react'
 
 export default function Page() {
-  return <div></div>
+
+  return (
+    <div className={styles.dashboardWrapper}>
+      <AdminNavbar />
+      <div className={styles.backgroundWrapper}>
+        <div className={styles.backgroundImage}>
+          <Image src={imgBackground} alt="Background image" quality={100} fill />
+        </div>
+      </div>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <div className={styles.events}>
+            Events
+          </div>
+          <div className={styles.stats}>
+            Statistics
+          </div>
+          <div className={styles.account}>
+            My Account
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
