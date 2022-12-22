ed09823e9aadfd898d272bcf4d48bbe7377834d6
Karsten Koerner <kgk1999@gmail.com>
2022-12-22 18:56:29 -0500

    testpage

---
+++ app/testpage/layout.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import AdminNavbar from '../../components/AdminNavbar'
+
+export default function Layout({ children }: any) {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  )
+}
