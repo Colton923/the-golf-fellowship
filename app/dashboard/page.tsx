d8d46ffb07f13d47b4349aa1c63facf2a6817de5
Colton923 <110353948+Colton923@users.noreply.github.com>
2022-12-24 11:48:12 -0500

    fixed shit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,10 @@
 import Image from 'next/image'
 import imgBackground from '../../public/static/images/tgf_dashboard_background.png'
 import styles from '../../styles/Dashboard_new.module.css'
-import AdminNavbar from '../../components/AdminNavbar'
+
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { Calendar } from '../../components/calendar/Calendar'
 
 export default function Page() {
   const [events, setEvents] = useState(false)
@@ -12,90 +13,125 @@ export default function Page() {
   const [account, setAccount] = useState(false)
   const [calendar, setCalendar] = useState(false)
   const [placeholder, setPlaceholder] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(365)
+
+  useEffect(() => {
+    const handleScreenResize = () => {
+      setScreenWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleScreenResize)
+    return () => window.removeEventListener('resize', handleScreenResize)
+  }, [])
+
+  const hideAllButOne = (one: string) => {
+    if (one === 'events') {
+      setStats(false)
+      setAccount(false)
+      setCalendar(false)
+      setPlaceholder(false)
+      setEvents(true)
+    } else if (one === 'stats') {
+      setEvents(false)
+      setAccount(false)
+      setCalendar(false)
+      setPlaceholder(false)
+      setStats(true)
+    } else if (one === 'account') {
+      setEvents(false)
+      setStats(false)
+      setCalendar(false)
+      setPlaceholder(false)
+      setAccount(true)
+    } else if (one === 'calendar') {
+      setEvents(false)
+      setStats(false)
+      setAccount(false)
+      setPlaceholder(false)
+      setCalendar(true)
+    } else if (one === 'placeholder') {
+      setEvents(false)
+      setStats(false)
+      setAccount(false)
+      setCalendar(false)
+      setPlaceholder(true)
+    } else {
+      setEvents(false)
+      setStats(false)
+      setAccount(false)
+      setCalendar(false)
+      setPlaceholder(false)
+    }
+  }
 
   // Handlers for opening the cards on click
   const handleEventsClick = () => {
-    if (events) {
-      setEvents(false)
-    } else {
-      setEvents(true)
-    }
+    events ? hideAllButOne('') : hideAllButOne('events')
   }
 
   const handleStatsClick = () => {
-    if (stats) {
-      setStats(false)
-    } else {
-      setStats(true)
-    }
+    stats ? hideAllButOne('') : hideAllButOne('stats')
   }
 
   const handleAccountClick = () => {
-    if (account) {
-      setAccount(false)
-    } else {
-      setAccount(true)
-    }
+    account ? hideAllButOne('') : hideAllButOne('account')
   }
 
   const handleCalendarClick = () => {
-    if (calendar) {
-      setCalendar(false)
-    } else {
-      setCalendar(true)
-    }
+    calendar ? hideAllButOne('') : hideAllButOne('calendar')
   }
 
   const handlePlaceholderClick = () => {
-    if (placeholder) {
-      setPlaceholder(false)
-    } else {
-      setPlaceholder(true)
-    }
+    placeholder ? hideAllButOne('') : hideAllButOne('placeholder')
   }
 
   return (
-    <div className={styles.dashboardWrapper}>
-      <AdminNavbar />
+    <div className={styles.main}>
       <div className={styles.backgroundWrapper}>
         <div className={styles.backgroundImage}>
           <Image src={imgBackground} alt="Background image" quality={100} fill />
         </div>
       </div>
-      <div className={styles.contentWrapper}>
-        <div className={styles.content}>
-          <div className={styles.buttonsWrapper}>
-            <div className={styles.events} onClick={handleEventsClick}>
-              Events
+      <div className={styles.dashboardWrapper}>
+        <div className={styles.contentWrapper}>
+          <div
+            className={styles.content}
+            style={{
+              flexDirection: screenWidth < 1000 ? 'column' : 'row',
+            }}
+          >
+            <div className={styles.buttonsWrapper}>
+              <div className={styles.events} onClick={handleEventsClick}>
+                Events
+              </div>
+              <div className={styles.stats} onClick={handleStatsClick}>
+                Statistics
+              </div>
+              <div className={styles.account} onClick={handleAccountClick}>
+                My Account
+              </div>
+              <div className={styles.account} onClick={handleCalendarClick}>
+                Calendar
+              </div>
+              <div className={styles.account} onClick={handlePlaceholderClick}>
+                Placeholder
+              </div>
             </div>
-            <div className={styles.stats} onClick={handleStatsClick}>
-              Statistics
-            </div>
-            <div className={styles.account} onClick={handleAccountClick}>
-              My Account
-            </div>
-            <div className={styles.account} onClick={handleCalendarClick}>
-              Calendar
-            </div>
-            <div className={styles.account} onClick={handlePlaceholderClick}>
-              Placeholder
-            </div>
-          </div>
-          <div className={styles.cardsWrapper}>
-            <div className={events ? styles.eventsCard : styles.hidden}>
-              events card
-            </div>
-            <div className={stats ? styles.statsCard : styles.hidden}>
-              stats card
-            </div>
-            <div className={account ? styles.accountCard : styles.hidden}>
-              account card
-            </div>
-            <div className={calendar ? styles.calendarCard : styles.hidden}>
-              calendar card
-            </div>
-            <div className={placeholder ? styles.placeholderCard : styles.hidden}>
-              placeholder card
+            <div className={styles.cardsWrapper}>
+              <div className={events ? styles.eventsCard : styles.hidden}>
+                events card
+              </div>
+              <div className={stats ? styles.statsCard : styles.hidden}>
+                stats card
+              </div>
+              <div className={account ? styles.accountCard : styles.hidden}>
+                account card
+              </div>
+              <div className={calendar ? styles.calendarCard : styles.hidden}>
+                <Calendar />
+              </div>
+              <div className={placeholder ? styles.placeholderCard : styles.hidden}>
+                placeholder card
+              </div>
             </div>
           </div>
         </div>
--- app/godaddy/Receipts.tsx
+++ app/godaddy/Receipts.tsx
@@ -2,11 +2,14 @@
 
 import { db } from '../../firebase/firebaseClient'
 import { collection, getDocs } from 'firebase/firestore'
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import styles from '../../styles/GoDaddy.module.css'
 import type { Order } from '../../types/order'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../../firebase/firebaseClient'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
 
 export const Receipts = () => {
   const [data, setData] = useState<Order[]>([])
@@ -15,38 +18,124 @@ export const Receipts = () => {
   const [totalSales, setTotalSales] = useState(0)
   const [totalTax, setTotalTax] = useState(0)
   const [totalSubtotal, setTotalSubtotal] = useState(0)
+  const [screenWidth, setScreenWidth] = useState(365)
+  const [screenHeight, setScreenHeight] = useState(500)
+
+  const [totalsColumnDefs, setTotalsColumnDefs] = useState([
+    { field: 'totalSubtotal' },
+    { field: 'totalTax' },
+    { field: 'totalSales' },
+  ])
+  const [totalsData, setTotalsData] = useState([] as any)
+  const gridRef = useRef<AgGridReact>(null)
+
+  //need to create a filter to sort the sales tax and sub total and order total
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'date', filter: 'agDateColumnFilter' },
+    { field: 'name', filter: 'agTextColumnFilter' },
+    { field: 'city', filter: 'agTextColumnFilter' },
+    { field: 'club', filter: 'agTextColumnFilter' },
+    { field: 'plan', filter: 'agTextColumnFilter' },
+    { field: 'term', filter: 'agTextColumnFilter' },
+    { field: 'status', filter: 'agTextColumnFilter' },
+    { field: 'sku', filter: 'agTextColumnFilter' },
+    { field: 'subTotal', filter: 'agNumberColumnFilter' },
+    { field: 'salesTax', filter: 'agNumberColumnFilter' },
+    { field: 'orderTotal', filter: 'agNumberColumnFilter' },
+  ])
+
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      minWidth: 100,
+      resizable: true,
+      sortable: true,
+      filter: true,
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 500) {
+        setScreenWidth(365)
+      } else if (window.innerWidth >= 500 && window.innerWidth < 700) {
+        setScreenWidth(500)
+      } else if (window.innerWidth >= 700 && window.innerWidth < 1000) {
+        setScreenWidth(700)
+      } else if (window.innerWidth >= 1000 && window.innerWidth < 1300) {
+        setScreenWidth(1000)
+      } else if (window.innerWidth >= 1300 && window.innerWidth < 1600) {
+        setScreenWidth(1300)
+      } else if (window.innerWidth >= 1600 && window.innerWidth < 2000) {
+        setScreenWidth(1600)
+      } else {
+        setScreenWidth(2000)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerHeight < 500) {
+        setScreenHeight(500)
+      } else if (window.innerHeight >= 500 && window.innerHeight < 700) {
+        setScreenHeight(500)
+      } else if (window.innerHeight >= 700 && window.innerHeight < 1000) {
+        setScreenHeight(700)
+      } else if (window.innerHeight >= 1000 && window.innerHeight < 1300) {
+        setScreenHeight(1000)
+      } else if (window.innerHeight >= 1300 && window.innerHeight < 1600) {
+        setScreenHeight(1300)
+      } else if (window.innerHeight >= 1600 && window.innerHeight < 2000) {
+        setScreenHeight(1600)
+      } else {
+        setScreenHeight(2000)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   useEffect(() => {
     if (data.length > 0) {
-      let total = 0
-      let tax = 0
-      let subtotal = 0
+      let tax = 0.0
+      let subtotal = 0.0
       data.forEach((item) => {
-        item.orderTotal
-          ? (total += parseInt(item.orderTotal.replace('$', '').replace(',', '')))
-          : (total += 0)
         item.salesTax
-          ? (tax += parseInt(item.salesTax.replace('$', '').replace(',', '')))
+          ? (tax += Number(item.salesTax.replace(/[^0-9.-]+/g, '')))
           : (tax += 0)
         item.subTotal
-          ? (subtotal += parseInt(item.subTotal.replace('$', '').replace(',', '')))
+          ? (subtotal += Number(item.subTotal.replace(/[^0-9.-]+/g, '')))
           : (subtotal += 0)
       })
-      setTotalSales(total)
+      tax = Math.round(tax * 100) / 100
+      subtotal = Math.round(subtotal * 100) / 100
+
+      setTotalSales(tax + subtotal)
       setTotalTax(tax)
       setTotalSubtotal(subtotal)
+      setTotalsData([
+        {
+          totalSubtotal: `$${subtotal}`,
+          totalTax: `$${tax}`,
+          totalSales: `$${tax + subtotal}`,
+        },
+      ])
     }
   }, [data])
 
   useEffect(() => {
     if (user) {
-      console.log(user)
       const getOrdersFromFirebase = async () => {
         const colRef = collection(db, 'goDaddy')
         const querySnapshot = await getDocs(colRef)
         querySnapshot.forEach(async (doc) => {
           const receipt = doc.data() as Order
-
           setData((prev) => [...prev, receipt])
         })
       }
@@ -57,68 +146,41 @@ export const Receipts = () => {
 
   return (
     <>
-      {loading || data.length === 0 ? (
+      {loading || data.length === 0 || totalsData.length === 0 ? (
         <div>Loading...</div>
       ) : (
-        <>
-          <div className={styles.main}>
-            <div className={styles.gridLayout}>
-              <div className={styles.gridHeaders}>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}></h1>
-                <h1 className={styles.itemHeader}>{totalSubtotal}</h1>
-                <h1 className={styles.itemHeader}>{totalTax}</h1>
-                <h1 className={styles.itemHeader}>{totalSales}</h1>
-              </div>
-              <div className={styles.gridHeaders}>
-                <h1 className={styles.itemHeader}>Date</h1>
-                <h1 className={styles.itemHeader}>Name</h1>
-                <h1 className={styles.itemHeader}>City</h1>
-                <h1 className={styles.itemHeader}>Club</h1>
-                <h1 className={styles.itemHeader}>Plan</h1>
-                <h1 className={styles.itemHeader}>Term</h1>
-                <h1 className={styles.itemHeader}>Status</h1>
-                <h1 className={styles.itemHeader}>SKU</h1>
-                <h1 className={styles.itemHeader}>Subtotal</h1>
-                <h1 className={styles.itemHeader}>Sales Tax</h1>
-                <h1 className={styles.itemHeader}>Order Total</h1>
-              </div>
-              {data.map((item, index) => (
-                <div className={styles.gridData} key={index}>
-                  <h1 className={styles.itemLabel}>{item.date ? item.date : ''}</h1>
-                  <h1 className={styles.itemLabel}>{item.name ? item.name : ''}</h1>
-                  <h1 className={styles.itemLabel}>{item.city ? item.city : ''}</h1>
-                  <h1 className={styles.itemLabel}>{item.club ? item.club : ''}</h1>
-                  <h1 className={styles.itemLabel}>{item.plan ? item.plan : ''}</h1>
-                  <h1 className={styles.itemLabel}>{item.term ? item.term : ''}</h1>
-                  <h1 className={styles.itemLabel}>
-                    {item.status ? item.status : ''}
-                  </h1>
-
-                  <h1 className={styles.itemLabel}>{item.sku ? item.sku : ''}</h1>
-
-                  <h1 className={styles.itemLabel}>
-                    {item.subTotal ? item.subTotal : ''}
-                  </h1>
-
-                  <h1 className={styles.itemLabel}>
-                    {item.salesTax ? item.salesTax : ''}
-                  </h1>
-
-                  <h1 className={styles.itemLabel}>
-                    {item.orderTotal ? item.orderTotal : ''}
-                  </h1>
-                </div>
-              ))}
+        <div className={styles.container}>
+          <div className={styles.totalsGridWrapper}>
+            <div
+              className="ag-theme-alpine"
+              style={{ width: screenWidth, height: 100 }}
+            >
+              <AgGridReact
+                rowData={totalsData}
+                columnDefs={totalsColumnDefs}
+                defaultColDef={defaultColDef}
+              />
             </div>
           </div>
-        </>
+          <div className={styles.gridWrapper}>
+            <div
+              className="ag-theme-alpine"
+              style={{
+                width: screenWidth,
+                height: screenHeight,
+                boxShadow: '5px 5px 10px 0 rgba(0,0,0,0.2)',
+                border: '1px solid rgba(0,0,0,0.2)',
+              }}
+            >
+              <AgGridReact
+                ref={gridRef}
+                rowData={data}
+                columnDefs={columnDefs}
+                defaultColDef={defaultColDef}
+              />
+            </div>
+          </div>
+        </div>
       )}
     </>
   )
