d3ec2923fc26825a1fbe3e8cf9546bf13537fe9e
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-02-15 10:04:26 -0500

    .

--- components/video/Video.tsx
+++ components/video/Video.tsx
@@ -3,13 +3,25 @@
 import { VideoProps } from '../../types/props'
 import styles from './Video.module.css'
 
+import { useState, useEffect } from 'react'
+
 const Video = (videoProps: VideoProps) => {
   const { source } = { ...videoProps }
+  const [videoSource, setVideoSource] = useState('')
+  const [videoLoaded, setVideoLoaded] = useState(false)
 
+  useEffect(() => {
+    setVideoSource(source)
+    setVideoLoaded(true)
+  }, [source])
+
+  if (!videoLoaded) {
+    return <div>Loading...</div>
+  }
   return (
     <div className={styles.videoWrapper}>
       <video className={styles.video} muted={true} controls={true}>
-        <source src={source} type="video/mp4" />
+        <source src={videoSource} />
       </video>
     </div>
   )
